e4b8e09ad9a80933cd001655cd7094b943fd57b8
BaiLiping <blp_engineer@outlook.com>
2022-11-28 13:04:19 +0800

    update

--- simulation/SPA/TrajectoryPMB-EOT-BP_without_extents/_common/resampleBernoulliTargets.m
+++
@@ -1,8 +0,0 @@
-function Bernoulli = resampleBernoulliTargets(Bernoulli,numParticles)
-
-indexes = resampleSystematic(Bernoulli.particleWeights,numParticles);
-Bernoulli.particleWeights = 1/numParticles*ones(numParticles,1);
-Bernoulli.particlesKinematic = Bernoulli.particlesKinematic(:,indexes);
-Bernoulli.particlesExtent = Bernoulli.particlesExtent(:,:,indexes);
-
-end
